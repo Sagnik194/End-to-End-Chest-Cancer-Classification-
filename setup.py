8f0dd49eba082860f8ffd897cb53eb3be6895fe9
sagnik sen <sensagnik42@gmail.com>
2026-05-07 12:32:09 +0530

    utils, exception & logger added

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "End-to-End-Chest-Cancer-Classification-"
-AUTHOR_USER_NAME = "entbappy"
+AUTHOR_USER_NAME = "Sagnik194"
 SRC_REPO = "cnnClassifier"
 AUTHOR_EMAIL = "sensagnik42@gmail.com"
 
